16940ec7112fce47c039aaef250c9d50c23fa47c
Daniil-Git <theartimen@gmail.com>
2023-12-13 14:35:05 +0200

    ss
    ss

--- app/src/main/java/com/example/androidstudioplanty/MainActivity.java
+++ app/src/main/java/com/example/androidstudioplanty/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int po=1;
-    int p=22;
+    int p=2209sss;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
