120d7443138d3c64cba309ff8aca275a706e1ff3
PicPew00 <102611511+PicPew00@users.noreply.github.com>
2023-12-29 01:15:27 +0200

    Implementin APIS

--- app/src/main/java/com/example/androidstudioplanty/AirQuality.java
+++ app/src/main/java/com/example/androidstudioplanty/AirQuality.java
@@ -1,4 +1,107 @@
 package com.example.androidstudioplanty;
 
-public class AirQuality {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AirQuality extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private Gson gson;
+
+
+
+    private TextView result79,result80,result70,result71;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_air_quality);
+
+        result79 = findViewById(R.id.textViewCafeteriaCO2Value); //Caffe CO2
+        result80 = findViewById(R.id.textViewCafeteriaTVOCValue); //Caffe TVOC
+
+
+        result71 = findViewById(R.id.textViewTVOCValue); //MDL TVOC Quality
+        result70 = findViewById(R.id.textViewCO2Value); //MDL CO2
+
+
+
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        //MDL
+        getSingleSensorData(79, result79);
+        getSingleSensorData(80, result80);
+        getSingleSensorData(70, result70);
+        getSingleSensorData(71, result71);
+
+
+
+        ImageView imageViewHome = findViewById(R.id.imageViewHome);
+        imageViewHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start MainActivity
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void getSingleSensorData(int sensorId, TextView resultTextView) {
+        String ENDPOINT = "https://mdl.frederick.ac.cy/cyriciotwebapis/api/Data/GetCardWidgetData"; // Replace with your API endpoint
+
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("id", sensorId);
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, ENDPOINT, requestBody,
+                    response -> {
+                        gson = new Gson();
+                        SingleValue sensorData = gson.fromJson(response.toString(), SingleValue.class);
+                        resultTextView.setText(String.format("%s%s", sensorData.getValue(), sensorData.getUnit()));
+                    },
+                    error -> {
+                        resultTextView.setText(error.getMessage());
+                    }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY3NjNDIzQGZyZWRlcmljay5hYy5jeSIsImp0aSI6ImY4ZmUzYmJmLTNjNDctNDBkMi05YzI1LWFjYjNkODIwODhmNiIsImVtYWlsIjoiYWNzYzQyM0BmcmVkZXJpY2suYWMuY3kiLCJ1aWQiOiIwODk1MDQ2NS01YmZlLTRjZjYtYjQwNS1mM2YyNjNjY2YxNDkiLCJSb2xlIjoiQ3VzdG9tZXIiLCJyb2xlcyI6IkN1c3RvbWVyIiwiZXhwIjoxNzM0NTE0MTcxLCJpc3MiOiJTZWN1cmVBcGkiLCJhdWQiOiJTZWN1cmVBcGlVc2VyIn0.LFkVpXuraX7r8xQJa_VNGMYzTx0LdGS5T0BAcrgt6TA");
+                    return headers;
+                }
+            };
+
+            requestQueue.add(request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/androidstudioplanty/MainActivity.java
+++ app/src/main/java/com/example/androidstudioplanty/MainActivity.java
@@ -3,71 +3,54 @@ package com.example.androidstudioplanty;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RequestQueue requestQueue;
-    private Gson gson;
-    private static final String ENDPOINT = "https://mdl.frederick.ac.cy/cyriciotwebapis/api/Data/GetCardWidgetData";
-
-    private TextView result;
-    public void onCardClick(View view) {
-        // Handle the card click event here
-        Intent intent = new Intent(this, Tempeature.class);
-        startActivity(intent);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tempeature);
-        result = findViewById(R.id.result);
-        requestQueue = Volley.newRequestQueue(this);
-        getSingleSensorData();
+        setContentView(R.layout.activity_main); // Use the correct layout that contains imageViewHome
+
+        ImageView imageViewHome = findViewById(R.id.imageViewHome);
+        imageViewHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start HomeActivity
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // Setup the OnClickListener for the Tempeature
+        MaterialCardView cardViewTemperature = findViewById(R.id.materialCardView4);
+        cardViewTemperature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start TemperatureActivity
+                Intent intent = new Intent(MainActivity.this, Tempeature.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        // Setup the OnClickListener for the Air Quality
+        MaterialCardView cardViewAirQuality= findViewById(R.id.materialCardView3);
+        cardViewAirQuality.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start Air Quality
+                Intent intent = new Intent(MainActivity.this, AirQuality.class);
+                startActivity(intent);
+            }
+        });
+        // Your other code here
     }
-
-    private void getSingleSensorData()  {
-
-        JSONObject requestBody = new JSONObject();
-        try {
-            requestBody.put("id",67);
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,ENDPOINT,requestBody, response -> {
-                gson = new Gson();
-                SingleValue sensorData = gson.fromJson(response.toString(),SingleValue.class);
-                result.setText(String.format("Value: %s%s", sensorData.getValue(), sensorData.getUnit()));
-            },error -> {
-                result.setText(error.getMessage());
-            }) {
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String> headers = new HashMap<String, String>();
-                    headers.put("Content-Type", "application/json");
-                    headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY3NjNDIzQGZyZWRlcmljay5hYy5jeSIsImp0aSI6ImY4ZmUzYmJmLTNjNDctNDBkMi05YzI1LWFjYjNkODIwODhmNiIsImVtYWlsIjoiYWNzYzQyM0BmcmVkZXJpY2suYWMuY3kiLCJ1aWQiOiIwODk1MDQ2NS01YmZlLTRjZjYtYjQwNS1mM2YyNjNjY2YxNDkiLCJSb2xlIjoiQ3VzdG9tZXIiLCJyb2xlcyI6IkN1c3RvbWVyIiwiZXhwIjoxNzM0NTE0MTcxLCJpc3MiOiJTZWN1cmVBcGkiLCJhdWQiOiJTZWN1cmVBcGlVc2VyIn0.LFkVpXuraX7r8xQJa_VNGMYzTx0LdGS5T0BAcrgt6TA");
-                    return headers;
-
-                }
-            };
-            requestQueue.add(request);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-    }
+    // Other methods as needed for your MainActivity
 }
