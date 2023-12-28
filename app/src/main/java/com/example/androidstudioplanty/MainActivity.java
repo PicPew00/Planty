e86a1e35c6e97235c4d1f038b6a4c2f372b70f15
PicPew00 <102611511+PicPew00@users.noreply.github.com>
2023-12-28 15:48:01 +0200

    commit

--- app/src/main/java/com/example/androidstudioplanty/MainActivity.java
+++ app/src/main/java/com/example/androidstudioplanty/MainActivity.java
@@ -1,17 +1,73 @@
 package com.example.androidstudioplanty;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    int po=1;
-    short t = 0;
-    int c=228;
+    private RequestQueue requestQueue;
+    private Gson gson;
+    private static final String ENDPOINT = "https://mdl.frederick.ac.cy/cyriciotwebapis/api/Data/GetCardWidgetData";
+
+    private TextView result;
+    public void onCardClick(View view) {
+        // Handle the card click event here
+        Intent intent = new Intent(this, Tempeature.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tempeature);
+        result = findViewById(R.id.result);
+        requestQueue = Volley.newRequestQueue(this);
+        getSingleSensorData();
+    }
+
+    private void getSingleSensorData()  {
+
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("id",67);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,ENDPOINT,requestBody, response -> {
+                gson = new Gson();
+                SingleValue sensorData = gson.fromJson(response.toString(),SingleValue.class);
+                result.setText(String.format("Value: %s%s", sensorData.getValue(), sensorData.getUnit()));
+            },error -> {
+                result.setText(error.getMessage());
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY3NjNDIzQGZyZWRlcmljay5hYy5jeSIsImp0aSI6ImY4ZmUzYmJmLTNjNDctNDBkMi05YzI1LWFjYjNkODIwODhmNiIsImVtYWlsIjoiYWNzYzQyM0BmcmVkZXJpY2suYWMuY3kiLCJ1aWQiOiIwODk1MDQ2NS01YmZlLTRjZjYtYjQwNS1mM2YyNjNjY2YxNDkiLCJSb2xlIjoiQ3VzdG9tZXIiLCJyb2xlcyI6IkN1c3RvbWVyIiwiZXhwIjoxNzM0NTE0MTcxLCJpc3MiOiJTZWN1cmVBcGkiLCJhdWQiOiJTZWN1cmVBcGlVc2VyIn0.LFkVpXuraX7r8xQJa_VNGMYzTx0LdGS5T0BAcrgt6TA");
+                    return headers;
+
+                }
+            };
+            requestQueue.add(request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/androidstudioplanty/SingleValue.java
@@ -0,0 +1,119 @@
+package com.example.androidstudioplanty;
+
+import com.google.gson.annotations.SerializedName;
+
+
+import java.util.List;
+
+public class SingleValue {
+    @SerializedName("widgetName")
+    private String widgetName;
+    @SerializedName("widgetType")
+    private Integer widgetType;
+    @SerializedName("widgetsId")
+    private Integer widgetsId;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("description")
+    private String description;
+    @SerializedName("icon")
+    private Object icon;
+    @SerializedName("value")
+    private Double value;
+    @SerializedName("imageIcon")
+    private String imageIcon;
+    @SerializedName("unit")
+    private String unit;
+    @SerializedName("lastUpdate")
+    private String lastUpdate;
+    @SerializedName("rules")
+    private List<Object> rules = null;
+
+    public String getWidgetName() {
+        return widgetName;
+    }
+
+    public void setWidgetName(String widgetName) {
+        this.widgetName = widgetName;
+    }
+
+    public Integer getWidgetType() {
+        return widgetType;
+    }
+
+    public void setWidgetType(Integer widgetType) {
+        this.widgetType = widgetType;
+    }
+
+    public Integer getWidgetsId() {
+        return widgetsId;
+    }
+
+    public void setWidgetsId(Integer widgetsId) {
+        this.widgetsId = widgetsId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Object icon) {
+        this.icon = icon;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getImageIcon() {
+        return imageIcon;
+    }
+
+    public void setImageIcon(String imageIcon) {
+        this.imageIcon = imageIcon;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(String lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public List<Object> getRules() {
+        return rules;
+    }
+
+    public void setRules(List<Object> rules) {
+        this.rules = rules;
+    }
+}
